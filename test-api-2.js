d520993089dd3a6114c43e0b02fc242571407f95
itsGabo22 <gabopaz2006@gmail.com>
2026-04-12 02:24:19 -0500

    chore: remove test scratch files exposing keys

--- test-api-2.js
+++
@@ -1,4 +0,0 @@
-fetch('https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyBF-7ZaWmrd3MbOXx2bDtwq0ZAc2P7xJZo')
-  .then(res => res.json())
-  .then(data => console.log(data.models?.map(m => m.name).filter(n => n.includes('gemini'))))
-  .catch(console.error);
--- test-api.js
+++
@@ -1,4 +0,0 @@
-fetch('https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyBF-7ZaWmrd3MbOXx2bDtwq0ZAc2P7xJZo')
-  .then(res => res.json())
-  .then(console.log)
-  .catch(console.error);
